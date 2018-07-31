123c343170167fe98101de7bcce404af427062bc
james947 <jmuriuki947@gmail.com>
2018-07-31 07:18:44 +0300

    ft(Crud)Enable crud functionality of posts - Add Post model why : To enable create, edit, destroy of blog posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,60 @@
 class HomeController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
   end
+
   def about
   end
+
+  def edit
+  end
+
   def newpost
+    @post = Post.newpost
   end
+
   def addpost
-    redirect_to root_path
+    @post = Post.newpost(post_params)
+    if @post.save
+      flash[:success] = "The post was created!" 
+      redirect_to @post
+    else 
+      render ‘newpost’
+    end 
   end
+
   def post
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Update successful" 
+      redirect_to @post
+    else
+      render ‘edit’
+    end
+end
+
+def destroy
+  @post.destroy
+  flash[:success] = "Post deleted" 
+  redirect_to root_path
+end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category_id, :author)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 
 end
+
