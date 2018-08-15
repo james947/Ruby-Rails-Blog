95026f8383072ac69d519711ccda35393f9bff93
james947 <jmuriuki947@gmail.com>
2018-08-15 11:02:10 +0300

    Create controllers for CRUD

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
     belongs_to :category
-    validates :title, :content, :category_id, :author presence: true
+    validates :title, :content, :category_id, :author, presence: true
 end
