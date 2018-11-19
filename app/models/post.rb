4f02976d2b51f2fb7df2e16bc5cf94e4ce101c97
james947 <jmuriuki947@gmail.com>
2018-11-19 13:28:00 +0300

    Enhance mobile responsiveness

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :category
   validates :title, :subtitle, :content, :category_id, :author, presence: true
   has_one_attached :image
+
+  def thumbnail
+    return self.image.variant(resize: '400x600').processed
+  end
+
+  def banner
+    return self.image.variant(resize: '300*300').processed
+  end
+  
 end
