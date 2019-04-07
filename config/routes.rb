67f507fed7c95d89bf157c0bc106bbac3af4474e
james947 <jmuriuki947@gmail.com>
2019-04-07 03:41:02 +0300

    Update main folder name

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'blog#index'
 
-  get '/posts/new' => 'home#newpost'
-  get '/posts' => 'home#posts'
-  get '/posts/:id' => 'home#show', as: 'show'
-  get '/posts/:id/edit' => 'home#edit', as: 'edit'
-  get '/about' => 'home#about'
-  post '/posts' => 'home#create', as: 'create'
-  patch '/posts/:id' => 'home#update', as: 'update'
-  delete '/posts/:id' => 'home#destroy', as: 'destroy'
+  get '/posts/new' => 'blog#newpost'
+  get '/posts' => 'blog#posts'
+  get '/posts/:id' => 'blog#show', as: 'show'
+  get '/posts/:id/edit' => 'blog#edit', as: 'edit'
+  get '/about' => 'blog#about'
+  post '/posts' => 'blog#create', as: 'create'
+  patch '/posts/:id' => 'blog#update', as: 'update'
+  delete '/posts/:id' => 'blog#destroy', as: 'destroy'
 end
